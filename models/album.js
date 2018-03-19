55df09a15559a0edb1f78a92ae83b8f02830e305
Daniele Galli <daniele.galli76@gmail.com>
2018-03-19 12:08:30 +0100

    varie

--- models/album.js
+++ models/album.js
@@ -1,4 +1,3 @@
-
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var group = require('./group').schema;
@@ -6,13 +5,13 @@ var genre = require('./genres').schema;
 var publisher = require('./publisher').schema;
 
 var AlbumSchema = new Schema({
-  titolo: String,
-  descrizione: String,
-  img: { data: String, contentType: String },
-  autore: [group],
-  genere: [genre],
-  anno: Number,
-  editore: [publisher],
+    titolo: String,
+    descrizione: String,
+    img: { data: String, contentType: String },
+    autore: [group],
+    genere: [genre],
+    anno: Number,
+    editore: [publisher],
 });
 
 module.exports = mongoose.model('album', AlbumSchema);
